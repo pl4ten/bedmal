944b6eafcb6cd49c94ce60eb8949dd4fbf21cff6
alirzex <alirzex.dev@gmail.com>
2021-08-02 19:30:12 +0430

    add to bag function completed

--- redux/store/store.utils.js
+++ redux/store/store.utils.js
@@ -1,47 +1,89 @@
-import {Alert} from 'react-native';
-import {jsonBeautify} from 'beautify-json';
+/* eslint-disable prettier/prettier */
+import { jsonBeautify } from 'beautify-json';
+
+const bagItemFinder = (bag, itemToFind) => {
+  let returnBody = {
+    exist: false,
+    bagIndex: null,
+    proIndex: null,
+  };
+  bag.forEach((item, index) => {
+    if (item.vendorID === itemToFind.vendorID) {
+      if (item.buyType === itemToFind.buyType && item.buyType === 'Pick Up') {
+        item.products.forEach((pro, proIndex) => {
+          if (pro.orderType === itemToFind.products[0].orderType) {
+            if (pro.packing === itemToFind.products[0].packing) {
+              if ( pro.selectedOption.label === itemToFind.products[0].selectedOption.label ) {
+                returnBody.exist = true;
+                returnBody.bagIndex = index;
+                returnBody.proIndex = proIndex;
+              } else {
+                returnBody.exist = false;
+                returnBody.bagIndex = index;
+                returnBody.proIndex = proIndex;
+              }
+            } else {
+              returnBody.exist = false;
+              returnBody.bagIndex = index;
+              returnBody.proIndex = proIndex;
+            }
+          } else {
+            returnBody.exist = false;
+            returnBody.bagIndex = index;
+            returnBody.proIndex = proIndex;
+          }
+        });
+      }
+      if (item.buyType === itemToFind.buyType && item.buyType === 'delivery') {
+        if (item.addressID === itemToFind.addressID) {
+          item.products.forEach((pro, proIndex) => {
+            if (pro.orderType === itemToFind.products[0].orderType) {
+              if (pro.packing === itemToFind.products[0].packing) {
+                if (
+                  pro.selectedOption.label ===
+                  itemToFind.products[0].selectedOption.label
+                ) {
+                  returnBody.exist = true;
+                  returnBody.bagIndex = index;
+                  returnBody.proIndex = proIndex;
+                } else {
+                  returnBody.exist = false;
+                  returnBody.bagIndex = index;
+                  returnBody.proIndex = proIndex;
+                }
+              } else {
+                returnBody.exist = false;
+                returnBody.bagIndex = index;
+                returnBody.proIndex = proIndex;
+              }
+            } else {
+              returnBody.exist = false;
+              returnBody.bagIndex = index;
+              returnBody.proIndex = proIndex;
+            }
+          });
+        }
+      }
+    }
+  });
+  return returnBody;
+};
 
 export const addItemToBag = (state, itemToAdd) => {
   let newBag = [];
   if (state.bag.length) {
     newBag = [...state.bag];
-    newBag.forEach(item => {
-      if (itemToAdd.vendorID === item.vendorID) {
-        item.cart.forEach(cart => {
-          if (cart.product.id === itemToAdd.cart[0].product.id) {
-            if (cart.addressID === itemToAdd.cart[0].addressID) {
-              if (cart.buyType === itemToAdd.cart[0].buyType) {
-                if (cart.price === itemToAdd.cart[0].price) {
-                  if (cart.orderType === itemToAdd.cart[0].orderType) {
-                    if (cart.packing === itemToAdd.cart[0].packing) {
-                      if (cart.selectedOption === itemToAdd.cart[0].selectedOption) {
-                        cart.quantity += parseFloat(cart.quantity + itemToAdd.cart[0].quantity);
-                      } else {
-                        newBag.push(itemToAdd);
-                      }
-                    } else {
-                      newBag.push(itemToAdd);
-                    }
-                  } else {
-                    newBag.push(itemToAdd);
-                  }
-                } else {
-                  newBag.push(itemToAdd);
-                }
-              } else {
-                newBag.push(itemToAdd);
-              }
-            } else {
-              newBag.push(itemToAdd);
-            }
-          } else {
-            item.cart.push(itemToAdd.cart[0]);
-          }
-        });
-      } else {
-        newBag.push(itemToAdd);
-      }
-    });
+    const existingItem = bagItemFinder(newBag, itemToAdd);
+    console.log(existingItem);
+    if ( existingItem.exist && existingItem.bagIndex !== null && existingItem.proIndex !== null ) {
+      newBag[existingItem.bagIndex].products[existingItem.proIndex].quantity += parseFloat(itemToAdd.products[0].quantity);
+    } else if (!existingItem.exist && existingItem.bagIndex !== null) {
+      newBag[existingItem.bagIndex].products.push(itemToAdd.products[0]);
+    } else if (!existingItem.exist && existingItem.bagIndex !== null && existingItem.proIndex !== null) {
+      newBag[existingItem.bagIndex].products[existingItem.proIndex].push(itemToAdd.products[0]);
+    } else {
+      newBag.push(itemToAdd);
+    }
     return newBag;
   } else {
     newBag = [...state.bag];
