bd5a65567ead3dfe41b61fef3d7482994c4e49c1
alirzex <alirzex.dev@gmail.com>
2021-08-14 16:33:21 +0430

    transaction completed

--- screen/account/styles/transaction.styles.js
+++ screen/account/styles/transaction.styles.js
@@ -1,4 +1,61 @@
 import {StyleSheet} from 'react-native';
 let _defz = require('../../com/def');
 
-export 
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#000',
+  },
+  content: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#FAFAFA',
+  },
+  card: {
+    marginTop: 20,
+    minHeight: 50,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+
+  },
+  cardTitle: {
+    fontFamily: 'FuturaPT-Book',
+    fontSize: 18,
+    color: '#707070',
+  },
+  cardFooter: {
+    fontFamily: 'FuturaPT-Book',
+    fontSize: 18,
+    color: '#C3BCBC',
+  },
+  info: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '90%',
+    alignSelf: 'center',
+    backgroundColor: '#fff',
+  },
+  infoText: {
+    fontFamily: 'FuturaPT-Book',
+    fontSize: 15,
+    color: '#707070',
+    marginVertical: 5,
+  },
+  infoTextBlue: {
+    fontFamily: 'FuturaPT-Book',
+    fontSize: 15,
+    color: '#3D80F2',
+  },
+  infoTextAddress: {
+    fontFamily: 'FuturaPT-Book',
+    fontSize: 15,
+    color: '#707070',
+    maxWidth: _defz.width / 1.5,
+  },
+  infoRight: {
+    marginLeft: _defz.width / 30,
+  },
+});
--- screen/account/transaction.js
+++ screen/account/transaction.js
@@ -1,16 +1,133 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, View, ScrollView, TouchableOpacity, Alert} from 'react-native';
+import {CardItem, Right, Left, Icon} from 'native-base';
+import {styles} from './styles/transaction.styles';
+
+import {selectUserToken} from '../../redux/user/user.selectors';
+import {connect} from 'react-redux';
+import {jsonBeautify} from 'beautify-json';
+import Loader from '../com/loader';
+import Headers from '../com/header';
+import Footers from '../com/footer';
+
+let _defz = require('../com/def');
 
 class Transaction extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      transaction: null,
+      vendors: null,
+    };
+  }
+
+  async getTransactions(id) {
+    try {
+      this.setState({isLoading: true});
+      await _defz
+        .get_via_token(
+          `user/account/transactions/info/${id}`,
+          'GET',
+          this.props.token,
+        )
+        .then(response => {
+          this.setState({isLoading: false});
+          console.log(jsonBeautify(response));
+          if (response.status === 200) {
+            this.setState({
+              transaction: response.transaction,
+              vendors: response.vendors,
+            });
+          }
+          if (response.status === 400) {
+            Alert.alert('Error', response.errors[0].message, [{text: 'ok'}], {
+              cancelable: true,
+            });
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentDidMount() {
+    let id = this.props.navigation.state.params.id;
+    this.getTransactions(id);
+  }
+
   render() {
     return (
-       <View>
-        <Text> textInComponent </Text>
+      <View style={styles.container}>
+        <Headers
+          route={'Transaction'}
+          navigation={this.props.navigation}
+          message
+        />
+
+        {this.state.isLoading ? (
+          <Loader />
+        ) : this.state.transaction ? (
+          <View style={styles.content}>
+            <View style={styles.info}>
+              <View style={styles.infoLeft}>
+                <Text style={styles.infoText}>Date</Text>
+                <Text style={styles.infoText}>Ref</Text>
+                <Text style={styles.infoText}>Total</Text>
+              </View>
+              <View style={styles.infoRight}>
+                <Text style={styles.infoText}>
+                  {this.state.transaction.created_at.slice(0, 10)}
+                </Text>
+                <Text style={styles.infoText}>
+                  {this.state.transaction.ref_id}
+                </Text>
+                <Text style={styles.infoText}>
+                  {this.state.transaction.total_price}
+                </Text>
+              </View>
+            </View>
+            <ScrollView>
+              <View>
+                {this.state.vendors.map(item => {
+                  return (
+                    <TouchableOpacity
+                      onPress={() =>
+                        this.props.navigation.navigate('order', {
+                          id: 'item.id',
+                        })
+                      }
+                      style={styles.card}>
+                      <Left>
+                        <Text style={styles.cardTitle}>{item.name}</Text>
+                        <Text style={styles.cardTitle}>
+                          £{item.total_price}
+                        </Text>
+                      </Left>
+
+                      <Right>
+                        <Icon
+                          type="AntDesign"
+                          name="arrowright"
+                          style={styles.icon}
+                        />
+                      </Right>
+                    </TouchableOpacity>
+                  );
+                })}
+              </View>
+              <View style={{marginTop: 100}} />
+            </ScrollView>
+          </View>
+        ) : null}
+
+        <Footers navigation={this.props.navigation} route={'account'} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
-
-export default Transaction;
+const mapStateToProps = state => ({
+  token: selectUserToken(state),
+});
+export default connect(mapStateToProps)(Transaction);
