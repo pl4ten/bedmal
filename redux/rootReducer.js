7b26841eee0c90e50788c82a552be20ec45891f1
alirzex <alirzex.dev@gmail.com>
2021-08-15 18:14:52 +0430

    ondemand reducer added

--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -5,16 +5,18 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import storeReducer from './store/store.reducer';
 import userReducer from './user/user.reducer';
+import onDemandReducer from './onDemand/onDemand.reducer';
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: ['store', 'user'],
+  whitelist: ['store', 'user', 'onDemand'],
 };
 
 const rootReducer = combineReducers({
   store: storeReducer,
   user: userReducer,
+  onDemand: onDemandReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
