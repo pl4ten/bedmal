96944ebad10b6104035a20aafb233d41e74d7a9e
alirzex <alirzex.dev@gmail.com>
2021-09-07 18:53:51 +0430

    statusbar added

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import {I18nManager} from 'react-native';
+import { name as appName } from './app.json';
+import { I18nManager, StatusBar } from 'react-native';
 
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import {store, persistor} from './redux/store';
+import { store, persistor } from './redux/store';
 
 const RNRedux = () => (
   <Provider store={store}>
     <PersistGate persistor={persistor}>
+      <StatusBar
+        backgroundColor="black"
+        setBackgroundColor="black"
+        networkActivityIndicatorVisible={true}
+      />
+
       <App />
     </PersistGate>
   </Provider>
@@ -19,5 +25,6 @@ const RNRedux = () => (
 
 I18nManager.allowRTL(false);
 I18nManager.forceRTL(false);
+
 console.disableYellowBox = true;
 AppRegistry.registerComponent(appName, () => RNRedux);
